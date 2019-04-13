1a87b4519a2880eef16ef3a002572704164eb2dc
melmarcondes <mel.marcondes@gmail.com>
2019-04-13 22:21:14 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -35,13 +35,9 @@ const redisPublisher = redisClient.duplicate();
 
 // Express route handlers
 
-app.get('/', (req, res) => {
-  res.send('Hi');
-});
-
 app.get('/values/all', async (req, res) => {
   const values = await pgClient.query('SELECT * from values');
-
+  console.log(values);
   res.send(values.rows);
 });
 
